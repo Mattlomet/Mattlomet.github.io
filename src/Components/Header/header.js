c9a43012631938404d2eb3d99359e35ea6b4f391
Mattlomet <mattlomet@live.com>
2019-01-29 14:37:34 -0500

    minor header tweaks

--- src/Components/Header/header.js
+++ src/Components/Header/header.js
@@ -1,15 +1,6 @@
 import React, { Component } from 'react';
 import BeachPic from "../../assets/beach.jpg"
-
-const zIndexZero = {
-  'z-index': 1,
-  'position': 'absolute',
-  'top': '15vw',
-  'right': '10vw'
-}
-const whiteText = {
-  'color': 'white'
-}
+import "./header.css"
 
 
 class Header extends Component {
@@ -17,16 +8,29 @@ class Header extends Component {
     return (
       <div className="hero is-small">
         <img src={BeachPic} alt="beach"/>
-        <div class="hero-body" style={zIndexZero} >
-          <div class="container">
-            <h1 class="title" style={whiteText}>
+        <div className="headerText">
+            <h1 className="title">
               Matt Lomet
       </h1>
-            <h2 class="subtitle" style={whiteText}>
+            <h2 className="subtitle" >
               Full Stack Web Developer
       </h2>
-          </div>
+          <div className="navbar">
+ 
+ <a href="#portfolio" className="navbarItem">
+ <i className="fas fa-laptop-code is-large"></i>        
+ </a>
+
+ <a href="#objective" className="navbarItem">
+ <i className="fas fa-male is-large"></i>
+ </a>
+
+ <a href="#contact" className="navbarItem">
+ <i className="fas fa-phone is-large"></i>
+ </a>
+ </div>
         </div>
+       
       </div>
     );
   }
