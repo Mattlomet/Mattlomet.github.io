87b09aebf0f713b7d55b6e0c69dfca0941b9a55e
Mattlomet <mattlomet@live.com>
2019-01-29 11:08:38 -0500

    inital build of website

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import BeachPic from "./assets/beach.jpeg"
+
+const zIndexZero = {
+  'z-index': 0,
+  'position': 'absolute'
+}
+const whiteText = {
+  'color': 'white'
+}
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App hero is-medium">
+        <img src={BeachPic} alt="beach" style={zIndexZero} />
+        <div class="hero-body" >
+          <div class="container">
+            <h1 class="title" style={whiteText}>
+              Matt Lomet
+      </h1>
+            <h2 class="subtitle" style={whiteText}>
+              Full Stack Web Developer
+      </h2>
+          </div>
+        </div>
       </div>
     );
   }
